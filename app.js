07739e9bcab3ea833cdb72ff803665f56a2b56d4
Iverson <iversonwang40@gmail.com>
2024-03-29 11:26:27 +0800

    removeDuplicates reduce

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
 
-// 設定首頁路由
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+const removeDuplicates = arr => {
+  const unique = arr.reduce((accu, curr) => {
+    const isExist = accu.includes(curr)
+    if (!isExist) accu.push(curr)
+    // if(accu.indexOf(curr) < 0) accu.push(curr)
+    return accu
+  }, [])
+  return unique
+}
 
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
+  const arr = ['b', 'a', 'c', 'a', 'b', 'b']
+  const arr2 = removeDuplicates(arr)
+  console.log('arr2', arr2)
 })
